f0463fc743b31fe77af326d86a2f1c275cc9c4c5
berryst <berryst@microsoft.com>
2016-04-29 15:14:50 +1200

    0.4
    nic

---
+++ sql-server-2014-alwayson-existing-vnet-and-ad/ARMDeploymentAO.ps1
@@ -0,0 +1,7 @@
+﻿Login-AzureRmAccount
+$ResourceGroupName = "sandboxao"
+$templateURI = "https://raw.githubusercontent.com/berryst/Templates/master/sql-server-2014-alwayson-existing-vnet-and-ad/azuredeploy.json"
+$templateParameterURI = "https://raw.githubusercontent.com/berryst/Templates/master/sql-server-2014-alwayson-existing-vnet-and-ad/azuredeploy.parameters.json"
+
+$ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name "AODeployment" -TemplateUri $templateURI  -TemplateParameterUri $templateParameterURI
