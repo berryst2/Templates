b883b5b5978047b3dcdacd638753d79c03084f64
berryst <berryst@microsoft.com>
2016-05-25 10:06:30 +1200

    parameters update

--- sql-server-2014-alwayson-existing-vnet-and-ad/ARMDeploymentAO.ps1
+++ sql-server-2014-alwayson-existing-vnet-and-ad/ARMDeploymentAO.ps1
@@ -2,6 +2,8 @@
 $ResourceGroupName = "sandboxao"
 $templateURI = "https://raw.githubusercontent.com/berryst/Templates/master/sql-server-2014-alwayson-existing-vnet-and-ad/azuredeploy.json"
 $templateParameterURI = "https://raw.githubusercontent.com/berryst/Templates/master/sql-server-2014-alwayson-existing-vnet-and-ad/azuredeploy.parameters.json"
-
-$ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName
+$RGLocation = "Australia Southeast"
+#$ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName
+$ResourceGroup = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $RGLocation
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -Name "AODeployment" -TemplateUri $templateURI  -TemplateParameterUri $templateParameterURI
+
